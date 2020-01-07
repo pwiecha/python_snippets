a415d6428d531ade36131c464505ceabd14b7b0c
pawel <pawel.wiecha@gmail.com>
2020-01-07 19:54:00 +0100

    More type hints testing

--- typing_test.py
+++ typing_test.py
@@ -9,17 +9,30 @@ def greeting(name: str) -> str:
     return 'Hello ' + name
 
 
+def no_return_type() -> None:
+    return
+
 # PEP 484:  when an argument is annotated as having type float,
 # an argument of type int is acceptable
-def sum_two(first_arg: float, second_arg: float) -> float:
-    return first_arg + second_arg
+# Also, how to mix type with default argument value
+def arith_op(first_arg: float, second_arg: float, op: str = 'add') -> float:
+    return first_arg + second_arg if op == 'add' else first_arg - second_arg
 
+# TODO list, tuples, dicts, classes and custom types + typing before assigning a value
 
 if __name__ == "__main__":
+    # passing
     greeting("Pawel")
+
+    arith_op(5, 5.1, 'add')
+    arith_op(10, 3.5, 'sub')
+
+    no_return_type()
+
+    # failing
     greeting(5)
     greeting(['foo', 'bar'])
-
-    sum_two(5, 5.1)
-    sum_two('a', 'b')
-    sum_two({'a': 5}, {4: 'bar'})
+    arith_op('a', 'b', 'c')
+    arith_op({'a': 5}, {4: 'bar'})
+    arith_op(10, 3.5, 3)
+    no_return_type(5)
