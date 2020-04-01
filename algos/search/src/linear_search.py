951c7daad0ae94822604d31eaf863e8779515b7b
p wiecha <pawel.wiecha@gmail.com>
2020-04-01 01:17:02 -0700

    type annotated linear search

---
+++ algos/search/src/linear_search.py
@@ -0,0 +1,20 @@
+''' Implementation of the linear search '''
+from mytypes import *
+
+def linear_search(target: TNum , sequence: Sequence[TNum]) -> int:
+    '''
+    Linear search algorithm
+    For sorted sequence of len n:
+    Best case O(1)
+    Worst case O(n)
+    Args:
+        target: an element to look for.
+        sequence: sequence of sorted data to look for element.
+    Returns:
+        Index of first element that matches the target
+        -1 if the element is missing in the sequence
+    '''
+    for index, element in enumerate(sequence):
+        if element == target:
+            return index
+    return -1  # Not found
