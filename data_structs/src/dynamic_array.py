66754b015d907e20258d7997f8690464e230d21d
pawel <pawel.wiecha@gmail.com>
2020-04-24 09:04:27 +0200

    working on dynamic array implementation

--- data_structs/src/dynamic_array.py
+++ data_structs/src/dynamic_array.py
@@ -3,38 +3,48 @@
    Implemented based on reference implementation from Goldwasser's book."""
 import ctypes
 
-class DynamicArray:
-    def __init__(self):
-        self._array = _make_array()
-        self._capacity = len(_array)
+class DynamicArray():
+    def __init__(self, capacity=2):
         self._itemcnt = 0
-    
+        self._capacity = capacity
+        self._array = _make_array(self._capacity)
+
+
+    def __len__(self):
+        return self._itemcnt
+
+
     def __getitem__(self, idx):
+        if 0 > idx <= 
         return self._array[idx]
 
+
     def __setitem__(self, idx, obj):
-        # add guard for wrong index w/ itemcnt
         if idx >= self._itemcnt:
             raise IndexError(f"{self.__name__} index out of range")
         self._array[idx] = obj
     
-    def __len__(self):
-        return len(_array)
+
 
     def append(self, obj):
         if self._itemcnt == self._capacity:
-            _resize()
-        self._array
-        #add obj to _array
-        #if out of space -> call _resize before
-        #update itemcount
+            self._resize()
+        self._array[self._itemcnt] = obj
+        self._itemcnt += 1
+
+    # TODO: add pop
 
 
-    def _make_array(self):
+    def _make_array(self, size):
         return # list w/ ctype objects - pointer like
 
+
     def _resize(self):
-        # create new, bigger array
+        self._new_arr = self._make_array(2*len(self._array))
+        self._new_arr[:len(self._array)] = self._array
+        self._array = self._new_arr
+        # create new, bigger array 2X
         # copy current references to the new array
+
 
     
