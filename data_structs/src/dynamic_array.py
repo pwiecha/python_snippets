d45e2e5c68bb1d03689e226b1add22bf54eb7e0d
pawel <pawel.wiecha@gmail.com>
2020-04-29 14:46:39 +0200

    testing and debugging dynamic_array - 90%

--- data_structs/src/dynamic_array.py
+++ data_structs/src/dynamic_array.py
@@ -3,7 +3,6 @@
 
 Implemented based on reference implementation from Goldwasser's book.
 """
-
 import ctypes
 
 
@@ -17,7 +16,7 @@ class DynamicArray():
         return self._itemcnt
 
     def __str__(self):
-        return f"{[item for item in self._array[:self._itemcnt]]}"
+        return f"[{', '.join(str(i) for i in self._array[:self._itemcnt])}]"
 
     def __getitem__(self, idx):
         if not 0 <= idx < self._itemcnt:
@@ -31,24 +30,31 @@ class DynamicArray():
 
     def append(self, obj):
         if self._itemcnt == self._capacity:
-            self._grow(2 * self._capacity)
+            self._resize(2 * self._capacity)
         self._array[self._itemcnt] = obj
         self._itemcnt += 1
 
     def pop(self):
-        if self._itemcnt <= 0:
-            raise IndexError(f"{self.__class__.__name__} index out of valid range")
-        popped = self._array[self._itemcnt]
-        self._itemcnt -= 1
-        return popped
+        try:
+            popped = self._array[self._itemcnt-1]
+            self._itemcnt -= 1
+            if self._capacity > 4 and self._itemcnt == self._capacity // 4:
+                self._resize(self._capacity // 2)
+            return popped
+        except (IndexError, ValueError) as err:
+            print(err)
+            raise # raise last exception in the current scope
 
-    def _grow(self, capacity):
+    def _resize(self, capacity):
         new_array = self._make_array(capacity)
-        new_array[:len(self._array)] = self._array
+        # Shrink
+        if len(self._array) > len(new_array):
+            new_array = self._array[:len(new_array)]
+        # Grow
+        else:
+            new_array[:len(self._array)] = self._array
         self._array = new_array
         self._capacity = capacity
-
-    # TODO: add shrink when _itemcnt < _capacity / 4, shrink by 2
 
     def _make_array(self, capacity):
         return (capacity * ctypes.py_object)()
--- data_structs/tests/test_dynamic_array.py
+++ data_structs/tests/test_dynamic_array.py
@@ -1,10 +1,11 @@
 import pytest
-import sys
 import random
 import math
 from data_structs.src.dynamic_array import DynamicArray
 
-# for debug: print(da, vars(da), sys.getsizeof(da), sys.getsizeof(da._array))
+# for debug:
+# import sys
+# print(da, vars(da), sys.getsizeof(da), sys.getsizeof(da._array))
 repeat_cnt = 50
 
 
@@ -17,6 +18,7 @@ def test_direct_append_grow():
     assert da._itemcnt == 2 and da._capacity == 2
     da.append(7)
     assert da._itemcnt == 3 and da._capacity == 4
+    print(da)
 
 def test_direct_get_set():
     da = DynamicArray()
@@ -29,11 +31,62 @@ def test_direct_get_set():
         da[i] = 10 - i
         assert da[i] == 10 - i
 
-def test_direct_illegal_get_set():
+@pytest.mark.repeat(repeat_cnt)
+def test_random_illegal_get_set():
+    da = DynamicArray()
+
+    repeat_val = random.randrange(10, 31)
+    val_above = repeat_val + random.randrange(1, 6)
+    val_below = random.randrange(-5, 0)
+    illegal_idx =  random.choice([val_above, val_below])
+    
+    for i in range(repeat_val):
+        da.append(i)
+    
+    with pytest.raises(IndexError):
+        da[illegal_idx]
+        da[illegal_idx] = random.randrange(0, 11)
 
 def test_direct_append_pop():
+    da = DynamicArray()
+
+    for _ in range(50):
+        rand_val = random.randrange(100)
+        itemcnt_pre_append = da._itemcnt
+        da.append(rand_val)
+        itemcnt_post_append = da._itemcnt
+        assert itemcnt_post_append == itemcnt_pre_append + 1
+        assert da.pop() == rand_val
+        itemcnt_post_pop = da._itemcnt
+        assert itemcnt_post_pop == itemcnt_post_append - 1
 
 def test_direct_illegal_pop():
+    da = DynamicArray()
+
+    with pytest.raises((IndexError, ValueError)):
+        da.pop()
+
+def test_direct_grow_shrink_array():
+    da = DynamicArray()
+
+    grow_idxs = [2**i + 1 for i in range(1, 7)]
+    print(grow_idxs)
+    for idx in range(1, (2**7)+1):
+        prev_capacity = da._capacity
+        da.append(idx)
+        new_capacity = da._capacity
+        if idx in grow_idxs:
+            assert new_capacity == 2 * prev_capacity
+
+    shrink_idxs = [32, 8]
+    print(da._itemcnt)
+    for idx in range((2**7), -1, -1):
+        prev_capacity = da._capacity
+        da.pop()
+        new_capacity = da._capacity
+        print(idx, prev_capacity, new_capacity)
+        #if idx in shrink_idxs:
+            #assert new_capacity == prev_capacity // 2
 
 
 @pytest.mark.repeat(repeat_cnt)
