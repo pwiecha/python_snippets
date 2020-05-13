75d8b934033f5d825c9e1185c873ed5a6d83f339
pawel <pawel.wiecha@gmail.com>
2020-05-13 12:35:03 +0200

    better implementation of the insertion sort

--- algos/sort/insertion_sort.py
+++ algos/sort/insertion_sort.py
@@ -1,21 +1,36 @@
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        #print(i)
-        if arr[i] < arr[i-1]:
-            #print(f"swapping {arr[i]}")
+# -*- coding: ascii -*-
+"""Implementations of the insertion sort: recursive and non-resursive."""
+from typing import Sequence, TypeVar
+TNum = TypeVar('TNum', int, float)
+
+
+def insertion_sort(seq: Sequence[TNum]) -> None:
+    for i in range(1, len(seq)):
+        if seq[i] < seq[i-1]:
             for j in range(i, 0, -1):
-                if arr[j] < arr[j-1]:
-                    arr[j], arr[j-1] = arr[j-1], arr[j]
+                if seq[j] < seq[j-1]:
+                    seq[j], seq[j-1] = seq[j-1], seq[j]
                 else:
                     break
 
+def insertion_sort_better(seq: Sequence[TNum]) -> None:
+    for i in range(1, len(seq)):
+        cur_elem = seq[i]
+        j: int = i
+        while j > 0 and seq[j-1] > cur_elem:
+            seq[j] = seq[j-1] # shift right
+            j -= 1
+        seq[j] = cur_elem # insert
+
+def insertion_sort_recursive(seq: Sequence[TNum]) -> None:
+    pass
+
 if __name__ == "__main__":
-    print("Test")
-    arr1=[3,1,5,2]
-    insertion_sort(arr1)
-    print(arr1, len(arr1))
+    sequence1=[3,1,5,2]
+    insertion_sort_better(sequence1)
+    print(sequence1, len(sequence1))
     import random
-    arr2 = random.sample(range(0, 30), 10)
-    print(arr2)
-    insertion_sort(arr2)
-    print(arr2)
+    sequence2 = random.sample(range(0, 30), 10)
+    print(sequence2)
+    insertion_sort_better(sequence2)
+    print(sequence2)
