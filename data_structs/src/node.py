a0c1e3a20694c82dcee760620003b359c8623066
p wiecha <pawel.wiecha@gmail.com>
2020-09-04 08:03:12 -0700

    Added Node ADT

---
+++ data_structs/src/node.py
@@ -0,0 +1,10 @@
+class Node(object):
+    """ Node object for building linked lists.
+        Using __slots__: fix number of attributes
+        for faster access and less memory (no __dict__)
+    """
+    __slots__ = '_element', '_next'
+
+    def init(self, _element, _next)
+        self._element = _element
+        self._next = _next
